86034ec5d0ef2cad443ad409ace08fe1d9bfaaaf
cxumol <cxumol@gmail.com>
2024-08-26 12:01:58 +0000

    i18n: update zh-hans and zh-hant

--- src/i18n/zh-hant.js
+++ src/i18n/zh-hant.js
@@ -1,2 +1,60 @@
 /* eslint-disable */
-export default {"env":{"system_init_message":"你是一個得力的助手"},"command":{"help":{"summary":"當前支持的命令如下：\n","help":"獲取命令幫助","new":"開始一個新對話","start":"獲取您的ID並開始一個新對話","img":"生成圖片，完整命令格式為`/img 圖片描述`，例如`/img 海灘月光`","version":"獲取當前版本號確認是否需要更新","setenv":"設置用戶配置，完整命令格式為/setenv KEY=VALUE","setenvs":"批量設置用户配置, 命令完整格式為 /setenvs {\"KEY1\": \"VALUE1\", \"KEY2\": \"VALUE2\"}","delenv":"刪除用戶配置，完整命令格式為/delenv KEY","clearenv":"清除所有用戶配置","system":"查看一些系統信息","redo":"重做上一次的對話 /redo 加修改過的內容 或者 直接 /redo","echo":"回显消息"},"new":{"new_chat_start":"開始一個新對話"}}}
+export default {
+  "env": {
+    "system_init_message": "你是得力助手"
+  },
+  "command": {
+    "help": {
+      "summary": "所有指令：\n",
+      "help": "查看可用指令。",
+      "act": "選擇要執行的操作。",
+      "mod_env_set": "設定用戶配置，`/mod_env_set KEY=VALUE`。",
+      "mod_env_set_batch": "批量設定用戶配置，`/mod_env_set_batch {\"KEY1\": \"VALUE1\", \"KEY2\": \"VALUE2\"}`。",
+      "mod_env_del": "刪除用戶配置，`/mod_env_del MY_KEY1`。",
+      "mod_env_del_all": "清除所有用戶配置。",
+      "mod_system": "顯示系統資訊。"
+    }
+  },
+  "acts": {
+    "flatter": {
+      "name": "讚美",
+      "prompt": "運用幽默和智慧，對用戶的每一句話都給予讚美和恭維，完全避免使用負面詞彙或評價。"
+    },
+    "criticize": {
+      "name": "批評",
+      "prompt": "採取嚴厲和批判的語氣，指出用戶輸入中的問題和缺陷，不給予任何讚揚或正面評價。"
+    },
+    "flirt": {
+      "name": "調情",
+      "prompt": "像真人一樣自然地與用戶調情。語氣輕鬆、有趣、浪漫，像LINE/WhatsApp聊天一樣，簡短甜蜜。不能表現出bot的蹤跡。"
+    },
+    "help": {
+      "name": "請求幫助",
+      "prompt": "你是得力助手"
+    },
+    "skim": {
+      "name": "總結",
+      "prompt": "將提供的內容的精髓提煉成簡潔的摘要，簡明扼要地突出關鍵要點和主要觀點。"
+    },
+    "q": {
+      "name": "提問",
+      "prompt": "根據提供的內容，提出三個批判性思考問題，挑戰假設並探究潛在的弱點，以及三個建設性問題，關注解決方案、改進和未來步驟。"
+    },
+    "chktruth": {
+      "name": "檢查真偽",
+      "prompt": "仔細檢查用戶輸入的真實性，考慮邏輯、推理和常識。闡明你的推理，並指出所提供信息中任何潛在的缺陷或不一致之處。"
+    },
+    "chkscam": {
+      "name": "反詐騙",
+      "prompt": "檢查用戶輸入，分析其中是否存在詐騙的疑點，考慮諸如不可能的承諾、索取個人數據、施壓策略以及可疑連結或附件等因素。根據你的推理，對詐騙的可能性進行清晰的評估。"
+    },
+    "ans": {
+      "name": "思考並回答",
+      "prompt": "仔細思考用戶的輸入，然後再表達你的意見或提供你的深思熟慮的答案。"
+    },
+    "explain": {
+      "name": "解釋",
+      "prompt": "闡明並解釋提供的內容。"
+    }
+  }
+}
